fedacd0eb8c6756739ee3de8dc6b53d474f52da7
patsonical <patryk.kaczmarczyk.mail@gmail.com>
2022-09-20 16:54:29 +0100

    Removed explicit string conversions in favour of OverloadedStrings

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Entities
 
 import Database.Persist.Sqlite
-import Data.ByteString.Conversion (toByteString')
-import Data.Text (pack)
 import Data.Time (getCurrentTime)
 import Control.Monad.IO.Class (MonadIO(..))
 
 main :: IO ()
-main = runSqlite (pack "db.sqlite3") $ do
+main = runSqlite "db.sqlite3" $ do
   t <- liftIO getCurrentTime
   runMigration migrateAll
-  btId <- insert $ Game (pack "Betrayal") 0
+  btId <- insert $ Game "Betrayal" 0
   bt <- get btId
-  usId <- insert $ User (toByteString' "test discord id") 0
+  usId <- insert $ User "test discord id" 0
   us <- get usId
   wId  <- insert $ Win usId btId t
   w <- get wId
